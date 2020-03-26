7c3827b036056b4326e21418ed542359e33b9c86
DELNEGEND <kiennguyen19323@gmail.com>
2020-03-26 11:18:16 +0700

    2.0

--- README.md
+++ README.md
@@ -1,56 +1,32 @@
 # toGoogleStyle.js
 
-## Link 2 script vào phần `<head></head>`
-### jQuery
-```html
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" crossorigin="anonymous"></script>
-```
+## Thêm script vào phần `<head></head>`
 ### toGoogleStyleCDN
 ```html
 <script src="https://cdn.jsdelivr.net/gh/delnegend/toGoogleStyle.js@1.0/toGoogleStyle.min.js" integrity="sha384-CsojGvi0BrX/Os+I75n6Tb3/ZgcazqptVr+FZjWCoxTPSXAC8ztx4/X8h4sbVvMW" crossorigin="anonymous"></script>
 ```
 
-## Tải phông chữ này xuống và sao chép vào thư mục gốc
-A. Bắt buộc<br>
-• [GoogleSans-Bold](https://github.com/DELNEGEND/toGoogleStyle.js/blob/master/GoogleSans-Bold.otf "Ấn vào để download")
+## Import phông bold trong thư mục GoogleSans vào website ([hướng dẫn](https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face))
 
-B. Tuỳ chọn<br>
-• [GoogleSans-Regular](https://github.com/DELNEGEND/toGoogleStyle.js/blob/master/GoogleSans-Regular.otf "Ấn vào để download")<br>
-• [GoogleSans-Italic](https://github.com/DELNEGEND/toGoogleStyle.js/blob/master/GoogleSans-Italic.otf "Ấn vào để download")
 
-## Thêm font vào HTML
-
-- Cách 1: sử dụng CSS *(recommended)*
-```css
-@font-face {
-  font-family: Google Sans;
-  src: url('./GoogleSans-Bold'),
-       url('./GoogleSans-Regular'),
-       url('./GoogleSans-Italic');
-}
-```
-- Cách 2: sử dụng [**loadExternalFont**](https://github.com/DELNEGEND/loadExternalFont.js "loadExternalFont repository").
-
-## Hướng dẫn sử dụng
-
-**1. Gán 1 class bất kỳ vào element chứa text cần convert thành GoogleStyle, ví dụ**
+## Gán 1 class bất kỳ vào element chứa text cần convert thành GoogleStyle, ví dụ**
 
 ```html
 <div class=".googleStyle">Google</div>
-<span class=".toGoogleStyle">Lorem Ipsum gì gì đấy...</span>
+<span class=".toGoogleStyle">Lorem Ipsum...</span>
 ```
 
-**2. Chạy hàm này trong phần footer với param như dưới**
+## Chạy hàm này
 ```javascript
-toGoogleStyle('<tên class>','<cỡ chữ>','<kiểu chữ>')
+toGoogleStyle('<tên class>','<cỡ chữ>'')
 ```
 - Tên class (string, bắt buộc): class vừa đặt ở bước trên
 - Cỡ chữ (integer, không bắt buộc): mặc định là 40
-- Kiểu chữ (string, không bắt buộc): mặc định là 'bold', có thể để thành 'regular', 'italic' hoặc 'bolditalic' nhưng phải copy thêm 2 font đã đề cập trên
 
 *Ví dụ*
 ```Javascript
-toGoogleStyle('.googleStyle')
-toGoogleStyle('.googleStyle',69)
-toGoogleStyle('.toGoogleStyle',96,'italic')
+toGoogleStyle('googleStyle')
+toGoogleStyle('toGoogleStyle',96)
 ```
+
+### Trên này là guild cơ bản nhất, còn muốn chuyên sâu hơn như chỉnh kiểu chữ in nghiêng + đậm... bạn có thể edit file toGoogleStyle.js và tự host.
--- toGoogleStyle.js
+++ toGoogleStyle.js
@@ -1,97 +1,77 @@
-// Main function
-const _toGoogleStyle = selector => {
+// jshint esversion: 9
+const toGoogleStyle = {
+  colorList: ["4285F4", "EA4335", "FBBC05", "4285F4", "34A853", "EA4335"],
+  _toGoogleStyle: selector => {
+    // Take content inside div/span.selector and convert it into string, I called this the selected string
+    let inputString = selector.innerHTML,
+      // Temporary array that will be converted into string to replace the selected string
+      finalHTMLOutput = [];
+    // Pointer of the colorList
+    let colorIndex = 0;
 
-  // Take content inside div/span.selector and convert it into string, I called this the selected string
-  let inputString = $(selector).html(),
-    // Temporary array that will be converted into string to replace the selected string
-    finalHTMLOutput = [],
-    // Color list
-    colorList = ["#4285F4", "#EA4335", "#FBBC05", "#4285F4", "#34A853", "#EA4335"];
+    // Loop for each letter in the selected string
+    for (let i = 0; i < inputString.length; i++) {
+      // Incrementing the color pointer
+      colorIndex++;
 
-  // Pointer of the colorList
-  let colorIndex = 0;
+      // Reset the pointer to 0 when it hit the final value in the color list
+      if (colorIndex == 6) (colorIndex -= 6);
 
-  // Loop for each letter in the selected string
-  for (let i = 0; i < inputString.length; i++) {
-    // Incrementing the color pointer
-    colorIndex++;
+      // Adding span and color to each individual letter
+      let currOutLetter = `<span class="_${toGoogleStyle.colorList[colorIndex]}">${inputString.charAt(i)}</span>`;
 
-    // Reset the pointer to 0 when it hit the final value in the color list
-    if (colorIndex == 6) (colorIndex -= 6);
-
-    // Adding span and color to each individual letter
-    let currOutLetter = `<span style="color: ${colorList[colorIndex]}">${inputString.charAt(i)}</span>`;
-    
-    // Push the individual letter to the temporary array
-    finalHTMLOutput.push(currOutLetter);
-  }
-  // Finally join all values in that array into one single string and replace the selected string
-  $(selector).html(finalHTMLOutput.join(""));
-};
-
-// Global style config like font, font-size, font-weight, font-style
-const googleStyleConfig = (selector, fontSize, fontStyle) => {
-  let weightOrDeco, _fw = 'font-weight',
-    _td = 'text-decoration';
-  switch (fontStyle) {
-    case 'regular':
-      weightOrDeco = '';
-      break;
-    case 'bold':
-      weightOrDeco = `${_fw}:bold;`;
-      break;
-    case 'italic':
-      weightOrDeco = `${_td}:italic;`;
-      break;
-    case 'bolditalic':
-      weightOrDeco = `${_fw}:bold;${_td}:italic;`;
-      break;
-    default:
-      weightOrDeco = "";
-      break;
-  }
-
-  // Completed CSS string
-  let googleStyleGlobalStylesheet = `${selector}>span{font-family:Google Sans;${weightOrDeco}font-size:${fontSize}px}`;
-
-  // Duh
-  let addCssToHTML = css => {
+      // Push the individual letter to the temporary array
+      finalHTMLOutput.push(currOutLetter);
+    }
+    // Finally join all values in that array into one single string and replace the selected string
+    selector.innerHTML = finalHTMLOutput.join("");
+  },
+  // Global style config like font, font-size, font-weight, font-style
+  googleStyleConfig: (selector, fontSize, fontStyle) => {
+    let weightOrDeco, _fw = 'font-weight',
+      _td = 'text-decoration';
+    switch (fontStyle) {
+      case 'regular':
+        weightOrDeco = '';
+        break;
+      case 'bold':
+        weightOrDeco = `${_fw}:bold;`;
+        break;
+      case 'italic':
+        weightOrDeco = `${_td}:italic;`;
+        break;
+      case 'bolditalic':
+        weightOrDeco = `${_fw}:bold;${_td}:italic;`;
+        break;
+      default:
+        weightOrDeco = "";
+        break;
+    }
+    let googleStyleGlobalStylesheet = `.${selector}>span{font-family:Google Sans;${weightOrDeco}font-size:${fontSize}px}`;
+    toGoogleStyle.addCSSToDOM(googleStyleGlobalStylesheet + toGoogleStyle.createColorCSS(toGoogleStyle.colorList));
+  },
+  addCSSToDOM: function (string) {
     let head = document.head || document.getElementsByTagName('head')[0],
       style = document.createElement('style');
     head.appendChild(style);
     style.type = 'text/css';
     if (style.styleSheet) {
-      style.styleSheet.cssText = css;
+      style.styleSheet.cssText = string;
     } else {
-      style.appendChild(document.createTextNode(css));
+      style.appendChild(document.createTextNode(string));
     }
-  };
-  addCssToHTML(googleStyleGlobalStylesheet);
-
-};
-
-/*
-
-If we include googleStyleConfig into _toGoogleStyle, there'll be 2 problems:
-Nếu nhét hàm googleStyleConfig vào _toGoogleStyle và dùng luôn hàm đó pass class vào, sẽ xảy ra 2 vấn đề
-1. Duplicated googleStyleConfig when the .google_style appear more than 1 time in DOM
-2. All tags with .google_style class will have the same innerHTML
-
-Example
-<div.google_style> ABC </div>
-<div.google_style> 123 </div>
-<div.google_style> XYZ </div>
-After running _toGoogleStyle('google_style')
-<div.google_style> ABC </div>
-<div.google_style> ABC </div>
-<div.google_style> ABC </div>
-
-The solution I know is to seperate the styleConfig(), passing class AND order into _toGoogleStyle one by one (can be done with .each() in jQuery)
-
-*/
-const toGoogleStyle = (selector, fontSize = 40, fontStyle = 'bold') => {
-  $(selector).each((index, val) => _toGoogleStyle(val));
-  googleStyleConfig(selector, fontSize, fontStyle);
+  },
+  createColorCSS: function (arr) {
+    let temp = [];
+    arr.forEach(elem => {
+      temp.push(`._${elem}{color:#${elem}}`);
+    });
+    return temp.join("");
+  },
+  init: function (selector, fontSize = 40, fontStyle = 'bold') {
+    document.querySelectorAll('.' + selector).forEach(elem => toGoogleStyle._toGoogleStyle(elem));
+    toGoogleStyle.googleStyleConfig(selector, fontSize, fontStyle);
+  },
 };
 
 // toGoogleStyle('.google_style');
